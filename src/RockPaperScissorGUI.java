304120e0db6c0090796e59669074fabf706dd31e
Simone <simohoff@gmx.de>
2026-01-09 12:07:27 +0100

    Initial commit

---
+++ src/RockPaperScissorGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+public class RockPaperScissorGUI extends JFrame {
+    public RockPaperScissorGUI() {
+    // GUI implementation for Rock Paper Scissor game
+    super("Rock Paper Scissor Game");
+
+    setSize(450, 580);
+
+    // Set layout to null for absolute positioning
+    setLayout(null);
+
+    // Load GUI in the center of the screen everytime we run the application
+    setLocationRelativeTo(null);
+    }
+}
